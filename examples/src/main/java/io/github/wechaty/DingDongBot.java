c7daf2cdbfa617d9fee5fec3ccc96532a37fa324
cunkoulaocai <1184016190@qq.com>
2020-07-06 21:58:33 +0800

    add log42j.xml add kotlin ding dong bot

--- examples/src/main/java/io/github/wechaty/DingDongBot.java
+++ examples/src/main/java/io/github/wechaty/DingDongBot.java
@@ -1,10 +1,15 @@
 package io.github.wechaty;
 
 
-import io.github.wechaty.user.Contact;
-import io.github.wechaty.user.Room;
+import io.github.wechaty.schemas.ContactQueryFilter;
+import io.github.wechaty.schemas.RoomMemberQueryFilter;
+import io.github.wechaty.user.*;
+import io.github.wechaty.user.manager.ContactManager;
+import io.github.wechaty.user.manager.RoomManager;
 import io.github.wechaty.utils.QrcodeUtils;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
 
 
 public class DingDongBot {
@@ -12,7 +17,7 @@ public class DingDongBot {
 
     public static void main(String[] args) throws Exception {
 
-        final String token = System.getenv("WECHATY_PUPPET_HOSTIE_TOKEN");
+        final String token = "your_token";
 
         if (StringUtils.isBlank(token)) {
 
@@ -24,28 +29,33 @@ public class DingDongBot {
         }
         Wechaty bot = Wechaty.instance(token);
 
-
         bot.onScan((qrcode, statusScanStatus, data) -> {
-            System.out.println(QrcodeUtils.getQr(qrcode));
-            System.out.println("Online Image: https://wechaty.github.io/qrcode/" + qrcode);
-        });
+                System.out.println(QrcodeUtils.getQr(qrcode));
+                System.out.println("Online Image: https://wechaty.github.io/qrcode/" + qrcode);
+            });
 
         bot.onMessage(message -> {
+            System.out.println(message.text());
 
             Contact from = message.from();
             Room room = message.room();
-
+            Contact to = message.to();
+            System.out.println("to:" + to.name());
             String text = message.text();
+            System.out.println(from.name());
+            RoomManager roomManager = new RoomManager(to.getWechaty());
+
 
             if (StringUtils.equals(text, "#ding")) {
                 if (room != null) {
                     room.say("dong");
                 } else {
-                    from.say("dong");
+                    // 想对面发消息
+                    from.say("一对一聊天");
+                    to.say("你好");
                 }
             }
         });
-
         bot.start(true);
 
     }
--- examples/src/main/kotlin/io/github/wechaty/ding-dong-bot.kt
+++ examples/src/main/kotlin/io/github/wechaty/ding-dong-bot.kt
@@ -1,1 +1,37 @@
-// To be write ...
+import io.github.wechaty.*
+import io.github.wechaty.schemas.ScanStatus
+import io.github.wechaty.user.ContactSelf
+import io.github.wechaty.user.Message
+import io.github.wechaty.utils.QrcodeUtils
+
+fun main() {
+    val token = "your_token";
+
+    val bot = Wechaty.instance(token)
+    bot.onScan(object : ScanListener {
+        override fun handler(qrcode: String?, statusScanStatus: ScanStatus, data: String?) {
+            println(qrcode?.let { QrcodeUtils.getQr(it) });
+            println("Online Image: https://wechaty.github.io/qrcode/" + qrcode);
+        }
+    })
+
+    bot.onLogin(object : LoginListener {
+        override fun handler(self: ContactSelf) {
+            println(self.name() + "登陆了")
+        }
+    })
+
+    bot.onMessage(object : MessageListener {
+        override fun handler(message: Message) {
+            val text = message.text()
+            val from = message.from()
+            if (from != null) {
+                println("收到来自" + from.name() + "的消息:" + text)
+            }
+        }
+    })
+
+    bot.start(true)
+}
+
+
